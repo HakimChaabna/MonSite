6b62e3ae005785fea86e09624fc2b579c940fa88
hakimchaabna <mchaabnahakim@gmail.com>
2025-12-25 15:37:54 +0100

    simplification CV

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -63,22 +63,3 @@ export const skills = [
 	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
 	// },
 ];
-
-export const publications = [
-	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
-	},
-	// {
-	// 	title: 'The Radiation of Uranium Compounds',
-	// 	authors: 'Marie Curie',
-	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
-	// 	time: '1898',
-	// 	link: '#',
-	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
-	// },
-];
